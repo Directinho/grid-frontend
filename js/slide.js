c4c2301d71410d254f9ef7d5216a871cae4d9784
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-11 11:36:09 -0300

    Fim da Primeira Aula

--- js/slide.js
+++ js/slide.js
@@ -1,1 +1,5 @@
+// Clonando o elemento HTML
 const cloneSlide = document.querySelector('.logos-slide').cloneNode(true);
+
+// Adicionando o clone slide dentro do Logos
+document.querySelector('.logos').appendChild(cloneSlide);
